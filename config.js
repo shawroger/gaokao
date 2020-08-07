ce96fe656754f75b78479ad06e3b13cb06854676
shawroger <roger@shawroger.com>
2020-08-07 21:24:55 +0800

    修改2020版本

--- config.js
+++ config.js
@@ -1,15 +1,31 @@
-var classMode = Array(16)
+const classMode = Array(16)
 	.fill(0)
 	.map((_, i) => ({
 		key: i + 1 + "班",
 		val: i + 1,
 	}));
 
-EasyTable.init()
+Matable.init({
+	title: "宁国中学高考查询",
+	countdown: [{
+		name: "2021 高考倒计时",
+		date: "2021-06-07 09:00:00"
+	}]
+})
+	.add({
+		index: false,
+		data: "./store/2020.csv",
+		title: "2020 高考录取查询",
+		index: false,
+		config: [
+			{ able: true, mode: null, label: "公告" },
+		],
+	})
 	.add({
 		index: true,
-		data: "./store/gk2019.csv",
+		data: "./store/2019.csv",
 		title: "2019 高考录取查询",
+		index: true,
 		config: [
 			{ able: false, mode: null, label: "序号" },
 			{
@@ -18,14 +34,15 @@ EasyTable.init()
 				mode: classMode,
 				label: "班级",
 			},
-			{ able: true, mode: "[]", label: "姓名" },
-			{ able: true, mode: "[]", label: "学校" },
+			{ able: true, mode: "[?]", label: "姓名" },
+			{ able: true, mode: "[?]", label: "学校" },
 		],
 	})
 	.add({
 		index: true,
-		data: "./store/gk2018.csv",
+		data: "./store/2018.csv",
 		title: "2018 高考录取查询",
+		footer: true,
 		config: [
 			{
 				able: true,
@@ -33,15 +50,16 @@ EasyTable.init()
 				sort: true,
 				label: "班级",
 			},
-			{ able: true, mode: "[]", label: "姓名" },
-			{ able: true, mode: "[]", label: "学校" },
+			{ able: true, mode: "[?]", label: "姓名" },
+			{ able: true, mode: "[?]", label: "学校" },
 		],
 	})
 	.add({
-		data: "./store/gk2018.score.csv",
+		data: "./store/2018-.csv",
 		title: "2018 高考理科成绩查询",
+		footer: true,
 		config: [
-			{ able: true, mode: "[]", label: "姓名" },
+			{ able: true, mode: "[?]", label: "姓名" },
 			{ able: true, mode: null, sort: true, label: "总分" },
 			{ able: true, mode: null, sort: true, label: "语文" },
 			{ able: true, mode: null, sort: true, label: "数学" },
